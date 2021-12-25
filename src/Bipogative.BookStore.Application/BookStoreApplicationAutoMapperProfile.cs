90921d6777f8c291c7058ce025010f15eaefbdab
Kaung Zaw Htet <kaungzawhtet.mm@gmail.com>
2021-12-25 10:59:28 +0630

    Mapping & Service Class at App Dir, Dtos&IService at contract

--- src/Bipogative.BookStore.Application/BookStoreApplicationAutoMapperProfile.cs
+++ src/Bipogative.BookStore.Application/BookStoreApplicationAutoMapperProfile.cs
@@ -1,4 +1,5 @@
 ﻿using AutoMapper;
+using Bipogative.BookStore.Books;
 
 namespace Bipogative.BookStore
 {
@@ -9,6 +10,9 @@ namespace Bipogative.BookStore
             /* You can configure your AutoMapper mapping configuration here.
              * Alternatively, you can split your mapping configurations
              * into multiple profile classes for a better organization. */
+
+              CreateMap<Book, BookDto>();
+              CreateMap<CreateUpdateBookDto, Book>(); // look weird
         }
     }
 }
